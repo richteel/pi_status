2fb359e8ab1ec281eb2ee807e32a4e26b536e8f9
Richard Teel <richteel@teelsys.com>
2025-02-17 15:20:40 -0500

    Updated launcher.sh to work with Raspbian Bookworm

--- display/launcher.sh
+++ display/launcher.sh
@@ -1,2 +1,5 @@
 cd /home/pi/pi_status/display
-PYTHONPATH=/usr/bin python3 /home/pi/pi_status/display/code.py -v
+# Uncomment the following line for Raspbian Bullseye
+# PYTHONPATH=/usr/bin python3 /home/pi/pi_status/display/code.py -v
+# Uncomment the following line for Raspbian Bookworm
+python3 -m venv env --system-site-packages && source env/bin/activate && python /home/pi/pi_status/display/code.py -v
